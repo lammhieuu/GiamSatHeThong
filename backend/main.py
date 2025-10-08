fee408da990ddf4d4e8882eaae66bad1efdf9a05
LamHieu <115642329+lammhieuu@users.noreply.github.com>
2025-10-08 08:50:15 +0700

    Update backend main.py,client monitor.py

--- backend/main.py
+++ backend/main.py
@@ -12,8 +12,9 @@ from datetime import datetime
 origins = [
     "https://monitor.lcit.vn:8000", 
     "http://localhost:8000",       
-    "http://127.0.0.1:8000",
-    "http://192.168.251.32:3002"
+    "http://127.0.0.1:3000",
+    "http://192.168.251.32:3002",
+    "http://localhost:3000"
 ]
 
 sio = socketio.AsyncServer(
